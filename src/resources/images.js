3eaa7fd1b4cd872c22e2e24e370bd4cf7c35e678
Draym <kevin.andres@epitech.eu>
2021-12-18 02:35:11 +0100

    resource images

--- src/resources/images.js
+++ src/resources/images.js
@@ -1,3 +1,7 @@
+import darkMatter from "./game/resource/dark_matter.png"
+import voidEssence from "./game/resource/void.png"
+import plasmaEnergy from "./game/resource/plasma.png"
+import uxonium from "./game/resource/uxonium.png"
 
 export const BackgroundImg = {
 
@@ -5,4 +9,11 @@ export const BackgroundImg = {
 
 export const GameGuiImg = {
 
+}
+
+export const GameResourceImg = {
+    DARK_MATTER: darkMatter,
+    VOID_ESSENCE: voidEssence,
+    PLASMA_ENERGY: plasmaEnergy,
+    UXONIUM: uxonium
 }
